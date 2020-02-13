2f260b515f4ba69cdb2677855d56457d1ec2aca7
LandonAM <46728053+LandonAM@users.noreply.github.com>
2020-02-12 21:00:50 -0500

    Added Greenwick

---
+++ MapFiles/panningGreen.js
@@ -0,0 +1,21 @@
+$(function() {
+  panZoomInstance = svgPanZoom('#svg-id', {
+    zoomEnabled: true,
+    controlIconsEnabled: true,
+    fit: true,
+    center: true,
+    maxZoom: 30,
+    minZoom: 0.1
+  });
+
+  // zoom out
+  panZoomInstance.zoom(5)
+
+  $("#move").on("click", function() {
+    // Pan by any values from -80 to 80
+    panZoomInstance.panBy({
+      x: Math.round(Math.random() * 160 - 80),
+      y: Math.round(Math.random() * 160 - 80)
+    })
+  });
+})
